4372b41e0f020b609c105b073e204fc81e621b69
Mike Wardo <mikewardo@Mikes-MacBook-Air.local>
2014-04-30 10:20:10 -0400

    Initial Commit

---
+++ CommandLineDuel/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  CommandLineDuel
+//
+//  Created by Mike Wardo on 4/30/14.
+//  Copyright (c) 2014 WARDMANM. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
